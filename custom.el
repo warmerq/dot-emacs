e35314f7d597db1dfc8a398dea9314ad3a297494
wumeng <wum@smartbow.net>
2019-12-11 15:39:56 +0800

    add more config

--- custom.el
+++ custom.el
@@ -3,9 +3,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-enabled-themes '(sanityinc-tomorrow-night))
- '(custom-safe-themes
-   '("06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" default))
+ '(all-the-icons-ivy-buffer-commands '(ivy-switch-buffer-other-window ivy-switch-buffer))
+ '(debug-on-error t)
  '(lsp-auto-guess-root nil)
  '(lsp-prefer-flymake nil)
  '(org-confirm-babel-evaluate nil)
@@ -16,11 +15,15 @@
  '(org-src-fontify-natively t)
  '(org-startup-folded 'overview)
  '(org-startup-indented t)
- '(org-tags-column 80))
+ '(org-tags-column 80)
+ '(package-selected-packages
+   '(lsp-origami better-shell smartparens iedit zerodark-theme zenburn-theme yasnippet-snippets which-key use-package try treemacs-projectile tern-auto-complete tao-theme rust-mode rg poet-theme origami org-bullets org noflet moe-theme magit lsp-ui js2-refactor hungry-delete htmlize grip-mode go-mode git-timemachine git-gutter flycheck faff-theme expand-region exec-path-from-shell dracula-theme doom-themes doom-modeline deadgrep dash-at-point counsel company-lsp company-emoji color-theme-sanityinc-tomorrow color-theme-modern cnfonts beacon base16-theme all-the-icons-ivy all-the-icons-dired alect-themes ac-js2))
+ '(rust-format-on-save t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(aw-leading-char-face ((t (:inherit ace-jump-face-foreground :height 3.0))))
  '(lsp-ui-doc-background ((t (:background nil))))
  '(lsp-ui-doc-header ((t (:inherit (font-lock-string-face italic))))))
