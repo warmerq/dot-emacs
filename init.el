323bcc65e24d9d832d340485004da2c754051479
wumeng <wum@smartbow.net>
2022-01-11 17:39:05 +0800

    for emacs-plus@28
    added org-roam and org-download

--- init.el
+++ init.el
@@ -17,4 +17,20 @@
 ;; mannually installed package would be in ./lisp
 (add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
 
+
+(defun add-tangle-headers ()
+  (message "running in %s" (buffer-file-name))
+  (cond
+   ;; ((f-ext? (buffer-file-name) "py")
+   ;;  (goto-char (point-min))
+   ;;  (insert "# This is a generated file do not edit\n"))
+   ((f-ext? (buffer-file-name) "el")
+    (goto-char (point-min))
+    (insert ";;; -*- lexical-binding: t -*-\n"))
+   (t
+    nil))
+  (save-buffer))
+
+(add-hook 'org-babel-post-tangle-hook 'add-tangle-headers)
+
 (org-babel-load-file (expand-file-name "~/.emacs.d/myinit.org"))
